38dd4c9494b6e7289db44dc2fb9b2c5d13189a98
Francheska <gffrancheska@outlook.com>
2017-03-15 22:01:29 -0400

    Updating

--- boo.js
+++ boo.js
@@ -1,14 +1,18 @@
-function Out() {
+function Run() {
 	document.getElementById("house").style.backgroundImage = "url('images/bg1.jpg')";
+	document.getElementById("cat").style.opacity = "0.0";
+	Boo1();
+	Boo2();
+	Boo3();
 }
 
 function Boo1() {
 	var elem = document.getElementById("firstGhost");
 	var pos = 0;
-	var id = setInterval(frame, .5);
+	var id = setInterval(frame, 0.5);
 
 	function frame() {
-		if (pos == 700) {
+		if (pos == 600) {
 			clearInterval(id);
 			document.getElementById("firstGhost").style.opacity = "0.0";
 			document.getElementById("firstGhost").style.transition = "5s";
@@ -30,7 +34,7 @@ function Boo2() {
 		if (pos == 400) {
 			clearInterval(id);
 			document.getElementById("secondGhost").style.opacity = "0.0";
-			document.getElementById("secondGhost").style.transition = "1s";
+			document.getElementById("secondGhost").style.transition = "0.5s";
 			In();
 		}
 		else {
@@ -60,9 +64,29 @@ function Boo3() {
 	}
 }
 
-function In(){
+function In() {
 	document.getElementById("house").style.backgroundImage = "url('images/bg2.jpeg')";
+	cat();
 	Boo4();
 	Boo5();
 	Boo6();
+}
+
+function cat() {
+	document.getElementById("cat").style.opacity = "1";
+
+	var elem = document.getElementById("cat");
+	var pos = 200;
+	var id = setInterval(frame, .5);
+
+	function frame() {
+		if (pos == 200) {
+			clearInterval(id);
+		}
+		else {
+			pos++;
+			elem.style.right = pos + 'px';
+			elem.style.left = pos + 'px';
+		}
+	}
 }
