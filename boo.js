c290663b3e8447d45ab570065255f686d0b6ece2
Francheska <gffrancheska@outlook.com>
2017-03-15 21:36:57 -0400

    Updating

--- boo.js
+++ boo.js
@@ -2,18 +2,16 @@ function Out() {
 	document.getElementById("house").style.backgroundImage = "url('images/bg1.jpg')";
 }
 
-function In(){
-	document.getElementById("house").style.backgroundImage = "url('images/bg2.jpeg')";
-}
-
 function Boo1() {
 	var elem = document.getElementById("firstGhost");
 	var pos = 0;
 	var id = setInterval(frame, .5);
 
 	function frame() {
-		if (pos == 600) {
+		if (pos == 700) {
 			clearInterval(id);
+			document.getElementById("firstGhost").style.opacity = "0.0";
+			document.getElementById("firstGhost").style.transition = "5s";
 		}
 		else {
 			pos++;
@@ -29,8 +27,11 @@ function Boo2() {
 	var id = setInterval(frame, .5);
 
 	function frame() {
-		if (pos == 100) {
+		if (pos == 400) {
 			clearInterval(id);
+			document.getElementById("secondGhost").style.opacity = "0.0";
+			document.getElementById("secondGhost").style.transition = "1s";
+			In();
 		}
 		else {
 			pos++;
@@ -46,8 +47,10 @@ function Boo3() {
 	var id = setInterval(frame, .5);
 
 	function frame() {
-		if (pos == -80) {
+		if (pos == 200) {
 			clearInterval(id);
+			document.getElementById("thirdGhost").style.opacity = "0.0";
+			document.getElementById("thirdGhost").style.transition = "5s";
 		}
 		else {
 			pos++;
@@ -55,4 +58,11 @@ function Boo3() {
 			elem.style.top= pos + 'px';
 		}
 	}
+}
+
+function In(){
+	document.getElementById("house").style.backgroundImage = "url('images/bg2.jpeg')";
+	Boo4();
+	Boo5();
+	Boo6();
 }
